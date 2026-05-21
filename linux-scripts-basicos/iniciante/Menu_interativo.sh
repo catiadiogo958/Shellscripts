0e5722cbb618a3cadc6aa5d368be0ab8693cb8fc
Catia Diogo <catiadiogo958@gmail.com>
2026-05-21 17:38:29 +0100

    Foram atualizadas as funcoes do Script Menu_Interativo

--- linux-scripts-basicos/iniciante/Menu_interativo.sh
+++ linux-scripts-basicos/iniciante/Menu_interativo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+clear
 Ver_data(){
 data_atual=` date`
 echo "Data atual $data_atual"
@@ -10,25 +10,28 @@ echo "Data atual $data_atual"
 
 menu_interativo(){
 while  true; do
+
 echo "----------------Menu interativo do utlizador------------"
 echo "        Escolha a opcao da tarefa que deseja fazer"
-echo "[1].Ver data e hora atual"
-echo "[2].Ver Utilizador atual"
-echo "[3].Ver Uso do disco"
-echo "[4].Ver uso da memoria"
-echo "[5].Ver processos em execucao"
-echo "[6].Ver ip da maquina"
+echo "[1].Ver Data e hora Atual"
+echo "[2].Ver Utilizador Atual"
+echo "[3].Ver Uso do Disco"
+echo "[4].Ver Uso da Memoria"
+echo "[5].Ver Processos em Execucao"
+echo "[6].Ver Ip da Maquina"
 echo "[7].Limpar o terminal"
 echo "[0].Sair"
 read -p "Escolha a opcao:" op
 
 case $op in 
-1)Ver_data ;;
-2)finger ;;
+1)Ver_data ;; 
+
+
+2)whoami ;; 
 3)df -h ;;
 4)free -h ;;
 5)ps aux ;;
-6)hostname ;;
+6)hostname -I ;;
 7)clear ;;
 0) echo "A sair..."; exit 0 ;;
 
